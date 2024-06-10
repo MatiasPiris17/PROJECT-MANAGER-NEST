0d32fe1a0c542250867988a658b19749a62bdc71
matias piris <matiaslinkedin17@gmail.com>
2024-06-10 18:29:06 -0300

    configuracion inicial api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
+import { CORS } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
+
+  app.use(morgan('dev'));
+
+  const configService = app.get(ConfigService);
+  console.log("Escuchando en el puerto: " + configService.get('PORT'))
+  console.log("Entorno: " + process.env.NODE_ENV)
+
+  app.enableCors(CORS)
+
+  app.setGlobalPrefix('api')
+
+  await app.listen(configService.get('PORT'));
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
