9130e40ba7389ecd0c403331db4c31de685f9509
matias piris <matiaslinkedin17@gmail.com>
2024-06-11 17:59:38 -0300

    add crud service

--- src/projects/services/projects.service.ts
+++ src/projects/services/projects.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { ProjectsEntity } from "../entities/projects.entity";
-import { Repository } from "typeorm";
+import { DeleteResult, Repository, UpdateResult } from "typeorm";
 
 
 @Injectable()
@@ -16,4 +16,55 @@ export class ProjectsService {
         return { "message": 'Hello World! Projects module' };
     }
 
+    public async createProject(body: ProjectsEntity) {
+        try {
+          return await this.projectRepository.save(body);
+        } catch (error) {
+          throw new Error(error);
+        }
+    }
+
+    public async findProjects() {
+        try {
+          return await this.projectRepository.find();
+        } catch (error) {
+          throw new Error(error);
+        }
+    }
+
+    public async findProjectById(id: string) {
+        try {
+          return await this.projectRepository
+            .createQueryBuilder('project')
+            .where({ id })
+            .getOne()
+        } catch (error) {
+          throw new Error(error);
+        }
+    }
+
+    public async updateProject(body: ProjectsEntity, id: string) {
+        try {
+          const project: UpdateResult = await this.projectRepository.update(id, body);
+          if (project.affected === 0) {
+            return undefined
+          }
+          return project
+        } catch (error) {
+          throw new Error(error);
+        }
+    }
+
+    public async deleteProject(id: string) {
+        try {
+          const project: DeleteResult = await this.projectRepository.delete(id);
+          if (project.affected === 0) {
+            return undefined
+          }
+          return project
+        } catch (error) {
+          throw new Error(error);
+        }
+    }
+    
 }
