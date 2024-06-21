bbf3c7bac14b0caa6769158d9863db31846cc254
matias piris <matiaslinkedin17@gmail.com>
2024-06-21 18:25:46 -0300

    add error manager service

--- src/projects/services/projects.service.ts
+++ src/projects/services/projects.service.ts
@@ -2,69 +2,93 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { ProjectsEntity } from "../entities/projects.entity";
 import { DeleteResult, Repository, UpdateResult } from "typeorm";
+import { ErrorManager } from "src/utils/error.manager";
 
 
 @Injectable()
 export class ProjectsService {
 
-    constructor(
-        @InjectRepository(ProjectsEntity)
-        private readonly projectRepository: Repository<ProjectsEntity>
-      ) { }
+  constructor(
+    @InjectRepository(ProjectsEntity)
+    private readonly projectRepository: Repository<ProjectsEntity>
+  ) { }
 
-    getHello(): object {
-        return { "message": 'Hello World! Projects module' };
-    }
+  getHello(): object {
+    return { "message": 'Hello World! Projects module' };
+  }
 
-    public async createProject(body: ProjectsEntity) {
-        try {
-          return await this.projectRepository.save(body);
-        } catch (error) {
-          throw new Error(error);
-        }
+  public async createProject(body: ProjectsEntity) {
+    try {
+      return await this.projectRepository.save(body);
+    } catch (err) {
+      throw new Error(err);
     }
+  }
 
-    public async findProjects() {
-        try {
-          return await this.projectRepository.find();
-        } catch (error) {
-          throw new Error(error);
-        }
-    }
+  public async findProjects() {
+    try {
+      const projects: ProjectsEntity[] = await this.projectRepository.find();
+      if (projects.length === 0) {
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'No se encontaron resultados',
+        })
+      }
 
-    public async findProjectById(id: string) {
-        try {
-          return await this.projectRepository
-            .createQueryBuilder('project')
-            .where({ id })
-            .getOne()
-        } catch (error) {
-          throw new Error(error);
-        }
+      return projects
+    } catch (err) {
+      throw ErrorManager.createSignatureError(err.message);
     }
+  }
 
-    public async updateProject(body: ProjectsEntity, id: string) {
-        try {
-          const project: UpdateResult = await this.projectRepository.update(id, body);
-          if (project.affected === 0) {
-            return undefined
-          }
-          return project
-        } catch (error) {
-          throw new Error(error);
-        }
-    }
+  public async findProjectById(id: string) {
+    try {
+      const project: ProjectsEntity = await this.projectRepository
+        .createQueryBuilder('project')
+        .where({ id })
+        .getOne()
 
-    public async deleteProject(id: string) {
-        try {
-          const project: DeleteResult = await this.projectRepository.delete(id);
-          if (project.affected === 0) {
-            return undefined
-          }
-          return project
-        } catch (error) {
-          throw new Error(error);
-        }
+      if (!project) {
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'No se encontaron resultados',
+        })
+      }
+
+      return project
+    } catch (err) {
+      throw ErrorManager.createSignatureError(err.message);
     }
-    
+  }
+
+  public async updateProject(body: ProjectsEntity, id: string) {
+    try {
+      const project: UpdateResult = await this.projectRepository.update(id, body);
+      if (project.affected === 0) {
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'No se pudo actualizar',
+        })
+      }
+      return project
+    } catch (err) {
+      throw ErrorManager.createSignatureError(err.message);
+    }
+  }
+
+  public async deleteProject(id: string) {
+    try {
+      const project: DeleteResult = await this.projectRepository.delete(id);
+      if (project.affected === 0) {
+        throw new ErrorManager({
+          type: 'BAD_REQUEST',
+          message: 'No se pudo eliminar',
+        })
+      }
+      return project
+    } catch (err) {
+      throw ErrorManager.createSignatureError(err.message);
+    }
+  }
+
 }
