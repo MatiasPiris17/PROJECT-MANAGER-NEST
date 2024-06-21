e4d5e92e2949859c3fbf5382a5d10d84e26c8ce7
matias piris <matiaslinkedin17@gmail.com>
2024-06-21 18:25:59 -0300

    controller projects basic

--- src/projects/controllers/project.controller.ts
+++ src/projects/controllers/project.controller.ts
@@ -1,13 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { ProjectsService } from '../services/projects.service';
+import { ProjectsEntity } from '../entities/projects.entity';
 
 @Controller('projects')
 export class ProjectsController {
-    
-    constructor(private readonly projectService: ProjectsService) {}
 
-    @Get() 
-    getHello(): object {
-      return this.projectService.getHello();
-    }
+  constructor(private readonly projectService: ProjectsService) { }
+
+  @Post('register')
+  public async createProject(@Body() body: ProjectsEntity) {
+    return await this.projectService.createProject(body);
+  }
+
+  @Get('all')
+  public async findProjects() {
+    return await this.projectService.findProjects();
+  }
+
+  @Get(':id')
+  public async findProjectById(@Param('id') id: string) {
+    return await this.projectService.findProjectById(id);
+  }
+
+  @Put('edit/:id')
+  public async updateProject(@Body() body: ProjectsEntity, @Param('id') id: string) {
+    return await this.projectService.updateProject(body, id);
+  }
+
+  @Delete('delete/:id')
+  public async deleteProject(@Param('id') id: string) {
+    return await this.projectService.deleteProject(id);
+  }
+
 }
