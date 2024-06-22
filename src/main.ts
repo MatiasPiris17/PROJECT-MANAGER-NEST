d4ee505e017d4637f24716baf712a9af90de4652
matias piris <matiaslinkedin17@gmail.com>
2024-06-22 10:47:13 -0300

    add interceptor with reflector

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import * as morgan from 'morgan';
 import { CORS } from './constants';
-import { ValidationPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(morgan('dev'));
-
-  const configService = app.get(ConfigService);
-  console.log("Escuchando en el puerto: " + configService.get('PORT'))
-  console.log("Entorno: " + process.env.NODE_ENV)
 
   /*AGREGAMOS UN PIPE PARA VALIDAR Y TRANSFORMAR LOS DATOS ANTES QUE LLEGUEN AL CONTROLADOR*/
   app.useGlobalPipes(
@@ -23,11 +19,20 @@ async function bootstrap() {
     })
   )
 
+  const reflector = app.get(Reflector); //ocultamos la passwrod
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
+
+  const configService = app.get(ConfigService);
+  console.log("Escuchando en el puerto: " + configService.get('PORT'))
+  console.log("Entorno: " + process.env.NODE_ENV)
+
   app.enableCors(CORS)
 
   app.setGlobalPrefix('api')
 
   await app.listen(configService.get('PORT'));
+
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
+
 bootstrap();
